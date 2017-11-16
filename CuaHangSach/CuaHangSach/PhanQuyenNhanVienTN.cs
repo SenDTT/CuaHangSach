9eb621aa65cfbcac376d4b547a52554e5eaf6705
CamTu <camtun80@gmail.com>
2017-11-16 11:11:35 +0700

    Phần đăng nhập

---
+++ CuaHangSach/CuaHangSach/PhanQuyenNhanVienTN.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuaHangSach
+{
+    public partial class PhanQuyenNhanVienTN : Form
+    {
+        public PhanQuyenNhanVienTN()
+        {
+            InitializeComponent();
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PhanQuyenNhanVienTN_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+
+            if (MessageBox.Show("Bạn chắc chắn muốn thoát chương trình?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
